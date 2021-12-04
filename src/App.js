7bb14978565aa684c6a5e5102c024dedb3b45194
Nhi Dinh <tuyetnhi1607@github.com>
2021-12-04 23:46:00 +0700

    cors

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import logo from "./logo.svg";
 import "./App.css";
+import {useEffect} from "react"
 import Login from "./components/auth/Login";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Register from "./components/auth/Register";
 import Table from "./components/table/Table";
 import Home from "./components/home/Home";
-
+const init = {
+    isLogined: false,
+    user: {
+      data: {
+        userName: "",
+        token: "",
+      },
+    },
+  };
 function App() {
+  if (!localStorage.getItem("auth")) {
+    const init = {
+      isLogined: false,
+      user: {
+        data: {
+          userName: "",
+          token: "",
+        },
+      },
+    };
+    localStorage.auth = JSON.stringify(init);
+  }
   const { isLogined } = JSON.parse(localStorage.auth).user;
   return (
     <div className="App">
